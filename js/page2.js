71e11a0c18ac813c81495aca70e8ee0a1e6a36dd
GemmaLP <gemmalouisaporter@outlook.com>
2022-10-12 18:21:59 +0100

    js link

--- js/page2.js
+++ js/page2.js
@@ -24,12 +24,12 @@ function selectPackaging() {
 
 function showSelection() {
   if(selectedFlavour == "Red Velvet" && selectedFrosting == "Vanilla" && selectedSprinkles == "Red Sprinkles") {
-    document.querySelector(".chosen_cake").src="/images/red-velvet-cupcake.jpg";
+    document.querySelector(".chosen_cake").src="./images/red-velvet-cupcake.jpg";
   } else if (selectedFlavour == "Chocolate" && selectedFrosting == "Vanilla" && selectedSprinkles == "Chocolate Drops") {
-    document.querySelector(".chosen_cake").src="/images/chocolate-cupcake.jpg";
+    document.querySelector(".chosen_cake").src="./images/chocolate-cupcake.jpg";
   } else if (selectedFlavour == "Vanilla" && selectedFrosting == "Lemon" && selectedSprinkles == "Rainbow Sprinkles") {
-    document.querySelector(".chosen_cake").src="/images/vanilla-cupcake.jpg";
+    document.querySelector(".chosen_cake").src="./images/vanilla-cupcake.jpg";
   } else {
-    document.querySelector(".chosen_cake").src="/images/cake first girls logo.png";
+    document.querySelector(".chosen_cake").src="./images/cake first girls logo.png";
   }
 }
